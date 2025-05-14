d18644707f314b4bb42bbf86f666269461b2c7b4
Adam Leadbetter <adamml@users.noreply.github.com>
2025-05-14 16:07:40 +0100

    Initial working code commit
    First commit of working code. A few items (spatial and temporal coverages and licenses) to be completed.

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# DCO-ODS to ODIS
+# DCO-ODS XLSX to ODIS
+
 Python script to take an XLSX spreadsheet prepared by the Decade Coordination Office for Ocean Data Sharing of metadata from Decade Actions and publish it the Ocean Data Information System federation
+
+- Author: [@adamml](https://github.com/adamml) for the [Decade Coordination Office for Ocean Data Sharing](https://oceandatasharing-dco.org/)
+
+## Dependencies
+- fastexcel==0.14.0
+- Jinja2==3.1.6
+- polars=1.29.0
+
+## Running the script
+
+```shell
+python xlsx2odis.py -h
+
+python xlsx2odis.py -b=BASE_URL -i=INPUT_FILE -o=OUT_DIR -s=INPUT_SHEET
+```
+
+Where:
+
+- `-b` specifies the base URL used to locate the output files on the web. Essentially, this is the web accessible location of the directory the script will write to and given by the `-o` option.
+- `-h` prints a manual or help page and exits the script with no further action taken.
+- `-i` specifies the XLSX file to use as an input to the script. The column structure expected in the XLSX file is detailed [here](XLSXSPEC.md).
+- `-o` specifies the output directory or folder to write the results of the script to.
+- `-s` specifies the sheet name within the input file given by `-i` where the content expected by the script is stored.
+
+## Ouputs
+
+1. One JSON-LD file per spreadsheet row
+2. One sitemap.xml file
+
+## Acknowledgement
+
+The work of the Decade Coordination Office for Ocean Data Sharing is supported by [Fondation Engie](https://fondation-engie.com).
---
+++ xlsx2odis.py
@@ -0,0 +1,142 @@
+import jinja2
+import json
+import polars
+import sys
+
+from typing import Dict, List, Optional
+
+
+def help():
+    """Displays a manual page for the script"""
+    print("""Usage xlsx2odis.py [options]
+
+Options:
+    -h             Displays this help page and exit
+    -b=BASE_URL    Specifies the root url, or BASE_URL, to locate outputs of
+                   this script on the web. This is essentially the web accesible
+                   address of DIRECTORY
+    -i=FILE        Use FILE as the input Excel spreadsheet
+    -o=DIRECTORY   Write output to DIRECTORY
+    -s=SHEETNAME   Read SHEET_NAME from the FILE specified by -i
+    """)
+
+
+def kwdBuilder(instr: str) -> List[str]:
+    """Helper function to turn multi-row text from an individual cell into a
+    list of keywords"""
+    kwd:List = []
+    try:
+        for kw in instr.split("\n"):
+            if kw != "":
+                kwd.append(kw)
+    except AttributeError:
+        pass
+    return kwd
+
+
+def getKeywords(row: Dict) -> List[str]:
+    """Helper function to build a List of keywords from an XLSX spreadsheet
+    row"""
+    kwd = []
+    for kw in kwdBuilder(row[26]):
+        kwd.append(kw)
+    for kw in kwdBuilder(row[20]):
+        kwd.append(kw)
+    for kw in kwdBuilder(row[27]):
+        kwd.append(kw)
+    return kwd
+
+
+def getVariablesMeasured(row: Dict) -> List[str]:
+    """Helper function to build a List of variables measured from an XLSX
+    spreadsheet row"""
+    varMeas = []
+    for vm in kwdBuilder(row[25]):
+        varMeas.append(vm)
+    return varMeas
+
+
+if __name__ == "__main__":
+    baseurl: Optional[str] = None
+    """The base URL to locate outputs on the web"""
+    infil: Optional[str] = None
+    """XLSX file to load data from"""
+    outdir: Optional[str] = None
+    """The directory to write output to"""
+    sheet: Optional[str] = None
+    """The sheet name to read from infil"""
+
+    # Parse the input options
+    for opt in sys.argv:
+        if opt.split("=")[0] == "-h":
+            help()
+            exit()
+        elif opt.split("=")[0] == "-b":
+            baseurl = opt.split("=")[1]
+            baseurl_ok: bool = False
+            # False if the base URL ends in a "/"
+            while not baseurl_ok:
+                if baseurl[-1] == "/":
+                    baseurl = baseurl[:-2]
+                else:
+                    baseurl_ok = True
+        elif opt.split("=")[0] == "-i":
+            infil = opt.split("=")[1]
+        elif opt.split("=")[0] == "-o":
+            outdir = opt.split("=")[1]
+        elif opt.split("=")[0] == "-s":
+            sheet = opt.split("=")[1]
+
+    # Handle errors on invocation
+    if baseurl is None:
+        raise Exception(
+            "No base URL specified, please use the -b=BASE_URL option")
+    if infil is None:
+        raise Exception(
+            "No input file specified, please use the -i=FILE option")
+    if outdir is None:
+        raise Exception(
+            "No output directory is specified, please use the -o=DIRECTORY",
+            " option")
+    if sheet is None:
+        raise Exception(
+            "No sheet name to be read from the input file specified, "
+            "please use the -s=SHEETNAME option")
+
+    #
+    # TODO: Add error checking on the existence on the input file and the
+    # accessibility of the output directoryß
+    #
+
+    # Read the input file with Polars
+    df: polars.DataFrame = polars.read_excel(source=infil, sheet_name=sheet)
+    """This dataframe holds the data from the input Excel sheet"""
+
+    # Set up the templating environment
+    environment = jinja2.Environment(
+        loader=jinja2.FileSystemLoader("templates/"))
+    template = environment.get_template("odis.jsonld")
+    template2 = environment.get_template("sitemap.xml")
+
+    # Set up the array to hold the sitemap
+    sitemap: List = []
+    
+    # Loop over the entries in the data
+    for row in df.iter_rows():
+        with open(f"{outdir}/{row[33]}.json", "w") as fil:
+            fil.write(template.render(
+                dsid=f"{baseurl}/{row[33]}.json",
+                name=row[8],
+                abstract=row[12],
+                kwds=getKeywords(row),
+                contentUrl=row[3],
+                orgName=row[14],
+                orgUrl=row[4],
+                variableMeasured=getVariablesMeasured(row)))
+            fil.close()
+        sitemap.append({"url": f"{baseurl}/{row[33]}.json"})
+
+    # Generate the sitemap
+    with open(f"{outdir}/sitemap.xml", "w") as fil:
+        fil.write(template2.render(sitemap=sitemap))
+        fil.close()        
